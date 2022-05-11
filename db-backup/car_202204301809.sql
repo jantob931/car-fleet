8baa4a163a9614f72fa73b25dacd62f3016ef102
jantob931 <j.tobiczyk43@gmail.com>
2022-05-11 07:09:18 +0200

    changing files

--- db-backup/car_202204301809.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO public.car (id,registration_number,brand,model) VALUES
-	 ('66','AVVD12','Fiat','Panda'),
-	 ('1b4621d5-6cde-4e07-ba25-4afbf7d3336b','AVasfasfVD12','assgdsfgsdfgd','Pandfagdfsgdsgasfasfda');
